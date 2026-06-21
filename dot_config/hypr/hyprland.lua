26977ce0a198898991a079189f11332fbc907dc7
Pavel Kirilin <s3riussan@gmail.com>
2026-06-21 15:16:49 +0200

    Changed hyprland config a bit.
    Signed-off-by: Pavel Kirilin <s3riussan@gmail.com>

--- dot_config/hypr/hyprland.lua
+++ dot_config/hypr/hyprland.lua
@@ -4,18 +4,19 @@
 ------------------
 ---- MONITORS ----
 ------------------
-function disable_hdmi()
-    hl.monitor({
-        output = "desc:Samsung Electric Company SAMSUNG 0x01000E00",
-        disabled = true
-    })
-end
 
-function enable_hdmi()
-    hl.monitor({
-        output = "desc:Samsung Electric Company SAMSUNG 0x01000E00",
-        disabled = false
-    })
+function HDMI_STATE(enabled)
+    if enabled then
+        hl.monitor({
+            output = "desc:Samsung Electric Company SAMSUNG 0x01000E00",
+            disabled = false
+        })
+    else
+        hl.monitor({
+            output = "desc:Samsung Electric Company SAMSUNG 0x01000E00",
+            disabled = true
+        })
+    end
 end
 
 hl.monitor({
@@ -28,15 +29,7 @@ hl.monitor({
 hl.monitor({
     output = "desc:Samsung Electric Company SAMSUNG 0x01000E00",
     mode = "3840x2160@60",
-    position = "-5760x0",
-    scale = 1,
-    cm = "auto",
-})
-
-hl.monitor({
-    output = "desc:ASUSTek COMPUTER INC VG279Q5R TALMDW005891",
-    mode = "1920x1080@200",
-    position = "0x0",
+    position = "-3840x0",
     scale = 1,
     cm = "auto",
 })
@@ -44,10 +37,19 @@ hl.monitor({
 hl.monitor({
     output = "desc:ASUSTek COMPUTER INC VG279Q5R TALMDW005892",
     mode = "1920x1080@200",
-    position = "-1920x0",
+    position = "0x0",
     scale = 1,
     cm = "auto",
 })
+
+hl.monitor({
+    output = "desc:ASUSTek COMPUTER INC VG279Q5R TALMDW005891",
+    mode = "1920x1080@200",
+    position = "1920x0",
+    scale = 1,
+    cm = "auto",
+})
+
 
 
 ---------------------
